8128e6f41ccbce597c2664f14c11d4249f81117b
Mairahenriquez <ch17013@ues.edu.sv>
2022-06-08 14:29:53 -0600

    Base template y style

--- DietasYNutricion/Apps/Paciente/models.py
+++ DietasYNutricion/Apps/Paciente/models.py
@@ -2,11 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class MDepartamento(models.Model):
-    codigo = models.CharField(max_length=4, primary_key=True)
-    nombre = models.CharField(max_length=50)
 
-class MMunicipio(models.Model):
-    codigo = models.CharField(max_length=4, primary_key=True)
-    nombre = models.CharField(max_length=50)
-    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
