cfe6042fc27e257ef01bf3b3b5a12272f0e572c6
pv17028 <101027617+pv17028@users.noreply.github.com>
2022-06-18 14:22:48 -0600

    Update urls.py

--- DietasYNutricion/Apps/Paciente/urls.py
+++ DietasYNutricion/Apps/Paciente/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('index/', login_required(views.index), name = "index"),
+    path('', login_required(views.index), name = "index"),
 ]
