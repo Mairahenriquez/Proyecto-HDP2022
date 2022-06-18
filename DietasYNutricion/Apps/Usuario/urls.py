08eebe93f9ac970ec5ead8ef2412c01cf3da3714
pv17028 <101027617+pv17028@users.noreply.github.com>
2022-06-18 14:13:44 -0600

    Add files via upload

--- DietasYNutricion/Apps/Usuario/urls.py
+++ DietasYNutricion/Apps/Usuario/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('Usuario',views.Usuarios.as_view(),name="Usuario"),
     path('registrar-usuario',views.RegistrarUsuarios.as_view(),name="RegistrarUsuario"),
     path('modificar-usuario/<int:pk>/',views.ModificarUsuarios.as_view(),name="ModificarUsuario"),
-    path('eliminar-usuario/<int:pk>/',views.EliminarUsuarios.as_view(),name="EliminarUsuario")
+    path('eliminar-usuario/<int:pk>/',views.EliminarUsuarios.as_view(),name="EliminarUsuario"),
 ]
