bfa85a6146aa652c1d405f12122e3515fb9a1416
pv17028 <101027617+pv17028@users.noreply.github.com>
2022-06-22 21:43:31 -0600

    Delete DietasYNutricion/Apps directory

--- DietasYNutricion/Apps/Paciente/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Paciente, Departamento, Municipio, PlanAlimenticio
-
-# Register your models here.
-
-admin.site.register(Paciente)
-admin.site.register(Departamento)
-admin.site.register(Municipio)
-admin.site.register(PlanAlimenticio)
